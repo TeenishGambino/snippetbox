19971d409ffd77daae51b948239350809762d2ca
Abiral Banjade <abiral.banjade.school@gmail.com>
2024-06-24 16:36:17 +0200

    Add : Routes.go, so that main is not too congested. Decoupling baby.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -50,43 +50,12 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	// if the user search /dab/static/something, the
-	// stripPrefix will take the /dab/ and replace /static/something with
-	// ./ui/static/something, and join them so that we have
-	// /dab/ui/static/something
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	//The handle functions make use of the Handler interface,
-	// normally you would have to do something like:
-	/*
-		type home struct {}
-		func (h *home) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("This is my home page"))
-		}
-
-		mux := http.NewServeMux() mux.Handle("/", &home{})
-	*/
-
-	// This is way to bloaty to do for many things,
-	// So we make use of http.HandlerFunc(home) which takes the contents in the home function
-	// And ServeHTTP just calls it.
-
-	// What we do here is just syntatic sugar so that we don't have to keep typing http.HandlerFunction all the time.
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	// We create a new server so that we can customize it
 	//We want to make use of our errorLogger, otherwise the listenAndServe using the default
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	// You have to dereference the value because the flag parser just has the location of it and not the value itself.
