1f94b8fff96ac2b5c2653a80e7a307c2f1365e33
Abiral Banjade <abiral.banjade.school@gmail.com>
2024-08-08 14:44:16 +0200

    Add : Caching relevant template files

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,6 +1,58 @@
 package main
 
-import "snippetbox.abiral.net/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"snippetbox.abiral.net/internal/models"
+)
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	//This is map that will be our cache
+	cache := map[string]*template.Template{}
+	// Use the filepath.Glob() function to get a slice of all filepaths that
+	// match the pattern "./ui/html/pages/*.tmpl.html". This will essentially gives
+	// us a slice of all the filepaths for our application 'page' templates
+	// like: [ui/html/pages/home.tmpl.html ui/html/pages/view.tmpl.html]
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		// Create a slice containing the filepaths for our base template, any
+		// partials and the page.
+		// files := []string{
+		// 	"./ui/html/base.tmpl.html",
+		// 	"./ui/html/partials/nav.tmpl.html",
+		// 	page,
+		// }
+		// Parse the base template file into a template set.
+		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseGlob() *on this template set* to add any partials.
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseFiles() *on this template set* to add the page template.
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the template set to the map, using the name of the page
+		// (like 'home.tmpl') as the key.
+		cache[name] = ts
+	}
+	return cache, nil
+}
 
 type templateData struct {
 	Snippet  *models.Snippet
