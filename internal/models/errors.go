984a33ddca0d8aedbcf70defd95250704978281d
Abiral Banjade <abiral.banjade.school@gmail.com>
2024-09-09 09:30:37 +0200

    Add : User, UserModel. Update application to have UserModel. Compiles, but not working

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -4,4 +4,12 @@ import (
 	"errors"
 )
 
-var ErrNoRecord = errors.New("models: no matching record found")
+// Declaring the variables
+// doing this is similar to just doing
+// var one at a time
+var (
+	ErrNoRecord = errors.New("models: no matching record found")
+	ErrInvalidCredentials = errors.New("models: ivalid credentials")
+	ErrDuplicateEmail = errors.New("models : duplicate email")
+)
+
